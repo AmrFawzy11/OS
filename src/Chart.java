a3b07976481f4ee51b49be4fb40dce8657575956
AmrFawzy11 <84874689+AmrFawzy11@users.noreply.github.com>
2022-04-18 02:45:05 +0200

    Update Chart.java

--- src/Chart.java
+++ src/Chart.java
@@ -14,40 +14,46 @@ import java.util.ArrayList;
 public class Chart {
     
     static String gantt (ArrayList<Job> p){
-        String graph = "";
+        Algorithmis.timing(p, Input.p);
+        String graph = Input.print3(Input.p) + "\n";
+        graph += "";
         
         // add top bar
         graph += " ";
         for(int i=0 ; i<p.size() ; i++){
-            for(int j=0; j<p.get(i).getBurst_time() ; j++)  graph += "--";
+            for(int j=0; j<8 ; j++)  graph += "--";
             graph += " ";
         }
         graph += "\n|";
         
         //adding process id in the middle
         for(int i=0 ; i<p.size() ; i++){
-            for(int j=0; j<p.get(i).getBurst_time()-1 ; j++)  graph += " ";
+            for(int j=0; j<7 ; j++)  graph += " ";
             if(p.get(i).getJob_number() != -1)  graph += "P";
             graph += p.get(i).getJob_number();
-            for(int j=0; j<p.get(i).getBurst_time()-1 ; j++)  graph += " ";
+            for(int j=0; j<7 ; j++)  graph += " ";
             graph += "|";
         }
         graph +="\n ";
         
         // adding bottom bar
         for(int i=0 ; i<p.size() ; i++){
-            for(int j=0; j<p.get(i).getBurst_time() ; j++)  graph += "--";
+            for(int j=0; j<8 ; j++)  graph += "--";
             graph += " ";
         }
         graph += "\n";
         
         // adding timeline
         graph += p.get(0).getStart_time();
-        int time =  p.get(0).getStart_time();;
+        float time =  p.get(0).getStart_time();;
         for(int i=0 ; i<p.size() ; i++){
-            for(int j=0; j<p.get(i).getBurst_time() ; j++)  graph += "  ";
+            for(int j=0; j<7 ; j++)  graph += "  ";
             time += p.get(i).getBurst_time();
             graph += time;
-        }return graph;
+        }
+        graph += "\n\n";
+        graph += "Average Turnarround time is " + (Algorithmis.total_turnAround_time)/Input.p.size() + "\n";
+        graph += "Average waiting time is " + (Algorithmis.total_waiting_time)/Input.p.size() + "\n";
+        return graph;
 }
 }
